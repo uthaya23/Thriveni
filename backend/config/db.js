123a21909c09b55ed4c18654c71b97fa91dc20c3
uthaya23 <113130843+uthaya23@users.noreply.github.com>
2026-06-27 12:59:48 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,25 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+// Create a cached variable to store the connection
+let cachedDb = null;
 
 const connectDB = async () => {
+  // If a connection already exists, reuse it!
+  if (cachedDb) {
+    console.log("Using existing database connection");
+    return Promise.resolve(cachedDb);
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    return conn;
+    // Save the connection to the cache
+    cachedDb = conn.connection; 
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return cachedDb;
   } catch (error) {
-    console.error('❌ MongoDB Connection Error:', error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
