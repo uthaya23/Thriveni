566bdb05e368ccbc860b87e19b73ac097c4de0d7
Uthaya <uthayadhoni76@gmail.com>
2026-06-30 15:59:25 +0530

    chore: remove ad hoc execution scripts

--- backend/scratch_check_roles.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const User = require('./models/User');
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const users = await User.find();
-  console.log(users.map(u => ({ name: u.name, username: u.username, role: u.role })));
-  process.exit(0);
-});
--- backend/scratch_check_template.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const CT = require('./models/ComponentTemplate');
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const t = await CT.findOne({ componentKey: 'EH5000_WHEEL_MOTOR' });
-  console.log(JSON.stringify(t, null, 2));
-  await mongoose.disconnect();
-});
